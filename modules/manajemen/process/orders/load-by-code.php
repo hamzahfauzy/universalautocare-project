85e36a8108442015d7eafb4911f30cdbdd1589a8
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-02-24 21:38:31 +0700

    update order

--- modules/manajemen/process/orders/load-by-code.php
+++ modules/manajemen/process/orders/load-by-code.php
@@ -5,7 +5,7 @@ use Core\Response;
 
 $db = new Database;
 
-$having = "HAVING trn_orders.id = $_GET[id]";
+$having = "HAVING trn_orders.code = '$_GET[code]'";
 
 $db->query = "SELECT trn_orders.*, mst_customers.name customer_name FROM trn_orders LEFT JOIN mst_customers ON mst_customers.id = trn_orders.customer_id $having";
 $orders = $db->exec('single');
