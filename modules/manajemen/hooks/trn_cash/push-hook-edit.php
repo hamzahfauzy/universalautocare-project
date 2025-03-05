eb103893610ca97eea790400d6f423d36b1bbb51
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-05 13:07:42 +0700

    cash edit

--- modules/manajemen/hooks/trn_cash/push-hook-edit.php
+++ modules/manajemen/hooks/trn_cash/push-hook-edit.php
@@ -6,3 +6,6 @@ $types = ['PENERIMAAN KAS' => 'cash_income', 'PENGELUARAN KAS' => 'cash_outcome'
 $cash_group = $_GET['filter']['cash_group'];
 Page::set_title(__('manajemen.label.'.$types[$cash_group]));
 Page::setActive('manajemen.'.$types[$cash_group]);
+
+Page::pushFoot('<script>window.cash_group = "'.$cash_group.'";</script>');
+Page::pushFoot('<script src="'.asset('assets/manajemen/js/create-cash.js').'"></script>');
