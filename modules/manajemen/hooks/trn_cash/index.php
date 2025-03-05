1eed14157281d000b9b11a2e59be072aa547b1ce
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-05 16:21:21 +0700

    add supplier name

--- modules/manajemen/hooks/trn_cash/index.php
+++ modules/manajemen/hooks/trn_cash/index.php
@@ -21,6 +21,7 @@ $this->db->query = "SELECT
                         $this->table.*,
                         CASE 
                             WHEN cash_group = 'PENERIMAAN KAS' THEN CONCAT(reference_number, ' / ', reference_date, '<br>', reference_name, ' / ', police_number_reference)
+                            WHEN cash_group = 'PENGELUARAN KAS' THEN CONCAT(reference_number, ' / ', reference_date, '<br>', reference_name)
                             ELSE reference_number
                         END reference_number,
                         CONCAT('DISC ', FORMAT(discount, 0), ' / ', FORMAT(total_payment, 0), '<br>', FORMAT(cash_total, 0)) total_payment
