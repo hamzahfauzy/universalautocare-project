673eacb33852e7d63989ebcde35248ad99a751eb
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-10 10:38:44 +0700

    oke lag fix

--- modules/manajemen/process/status/cash/cancel.php
+++ modules/manajemen/process/status/cash/cancel.php
@@ -13,7 +13,7 @@ $payment = $db->single('trn_cash', ['id' => $_GET['id']]);
 $paymentTable = $payment->cash_group == 'PENERIMAAN KAS' ? 'trn_orders' : 'trn_purchases';
 if(in_array($payment->cash_group, ['PENERIMAAN KAS', 'PENGELUARAN KAS']))
 {
-    $paymentData = $db->single($paymentTable, ['code' => $data->reference_number]);
+    $paymentData = $db->single($paymentTable, ['code' => $payment->reference_number]);
     $db->update($paymentTable,[
         'total_payment' => $paymentData->total_payment - $payment->total_payment
     ], [
