ce9585585acfbaeac9adeb2394099515c3275a6d
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-10 10:00:34 +0700

    update cancel

--- modules/manajemen/process/status/cash/cancel.php
+++ modules/manajemen/process/status/cash/cancel.php
@@ -10,17 +10,15 @@ $db->update('trn_cash', [
 ]);
 
 $payment = $db->single('trn_cash', ['id' => $_GET['id']]);
-
-if($payment->cash_group != 'BIAYA KAS')
+$paymentTable = $data->cash_group == 'PENERIMAAN KAS' ? 'trn_orders' : 'trn_purchases';
+if(in_array($payment->cash_group, ['PENERIMAAN KAS', 'PENGELUARAN KAS']))
 {
-    $total_payment = $payment->total_payment;
-    $reference_number = $payment->reference_number;
-    $tbl = ['PENERIMAAN KAS' => 'trn_orders', 'PENGELUARAN KAS' => 'trn_purchases'];
-    $db->query  = "Update ".$tbl[$payment->cash_group]."
-    Set total_payment = Coalesce(A.total_payment, 0) - $total_payment
-    Where code = $reference_number";
-
-    $db->exec();
+    $paymentData = $db->single($paymentTable, ['code' => $data->reference_number]);
+    $db->update($paymentTable,[
+        'total_payment' => $paymentData->total_payment - $data->total_payment
+    ], [
+        'code' => $data->reference_number
+    ]);
 }
 
 set_flash_msg(['success' => "Data berhasil dicancel"]);
