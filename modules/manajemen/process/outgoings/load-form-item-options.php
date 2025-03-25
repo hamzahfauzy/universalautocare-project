aade174e1b038bfb37f3ca1516490424516986a0
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-03-25 12:01:02 +0700

    update oke

--- modules/manajemen/process/outgoings/load-form-item-options.php
+++ modules/manajemen/process/outgoings/load-form-item-options.php
@@ -9,7 +9,12 @@ $filter  = Request::get('filter', []);
 
 $having = "HAVING category_id = $_GET[category_id]";
 
-$db->query = "SELECT * FROM mst_items $having";
+$db->query = "SELECT 
+                    *, 
+                    CASE WHEN item_type = 1 THEN price
+                         WHEN item_type = 0 THEN COALESCE((SELECT price FROM trn_purchase_items WHERE item_id = mst_items.id ORDER BY id DESC LIMIT 1), 0)
+                    END price
+                FROM mst_items $having";
 $products = $db->exec('all');
 
 return Response::json(compact('products'), 'item options loaded');
