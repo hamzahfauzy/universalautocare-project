eb854e53db4fc7e8da3c9026f383aad15c6798e7
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2025-04-15 16:01:06 +0700

    force redirect to dashbaord

--- modules/default/process/index.php
+++ modules/default/process/index.php
@@ -1,8 +1,4 @@
 <?php
 
-use Core\Page;
-
-Page::set_title(__("crud.label.home"));
-Page::setActive("crud.label.home");
-
-return view('default/views/index');
+header('location: manajemen/dashboard/index');
+die;
